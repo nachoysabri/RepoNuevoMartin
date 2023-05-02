54c952a3d1c94b763686dd2b81a61ec0a8b0df09
Re-Start <nachoysabrigithub@gmail.com>
2023-05-02 09:40:15 -0300

    intento inconcluso de carrousel en pstaña 2

---
+++ proyecto/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.slick-carousel').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
